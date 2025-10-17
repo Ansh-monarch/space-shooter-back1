4f1726bb9ee06708841e3445c7d49de1a188b78e
Ansh-monarch <anshoffi86@gmail.com>
2025-10-17 20:56:32 +0530

    Update game.js

--- game.js
+++ game.js
@@ -10,22 +10,30 @@ class Game {
     for (let i = 0; i < 5; i++) {
       this.createAsteroid();
     }
+    
+    console.log('🎮 Game instance created with 5 asteroids');
   }
   
   addPlayer(socketId) {
     this.players[socketId] = {
+      id: socketId,
       x: Math.random() * this.gameWidth,
       y: Math.random() * this.gameHeight,
       rotation: 0,
       velocity: { x: 0, y: 0 },
       health: 100,
       score: 0,
-      color: `hsl(${Math.random() * 360}, 100%, 50%)`
+      color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+      name: `Player${Object.keys(this.players).length + 1}`
     };
+    console.log(`👾 Player ${socketId} joined. Total players: ${Object.keys(this.players).length}`);
   }
   
   removePlayer(socketId) {
-    delete this.players[socketId];
+    if (this.players[socketId]) {
+      console.log(`👋 Player ${socketId} left. Total players: ${Object.keys(this.players).length - 1}`);
+      delete this.players[socketId];
+    }
   }
   
   updatePlayer(socketId, data) {
@@ -72,7 +80,7 @@ class Game {
   }
   
   createAsteroid() {
-    this.asteroids.push({
+    const asteroid = {
       x: Math.random() * this.gameWidth,
       y: Math.random() * this.gameHeight,
       velocity: {
@@ -82,12 +90,15 @@ class Game {
       size: 30 + Math.random() * 20,
       rotation: 0,
       rotationSpeed: (Math.random() - 0.5) * 0.1
-    });
+    };
+    this.asteroids.push(asteroid);
+    return asteroid;
   }
   
   update() {
     // Update bullets
-    this.bullets.forEach((bullet, index) => {
+    for (let i = this.bullets.length - 1; i >= 0; i--) {
+      const bullet = this.bullets[i];
       bullet.x += bullet.velocity.x;
       bullet.y += bullet.velocity.y;
       bullet.ttl--;
@@ -100,12 +111,12 @@ class Game {
       
       // Remove old bullets
       if (bullet.ttl <= 0) {
-        this.bullets.splice(index, 1);
+        this.bullets.splice(i, 1);
       }
-    });
+    }
     
     // Update asteroids
-    this.asteroids.forEach((asteroid, index) => {
+    this.asteroids.forEach((asteroid) => {
       asteroid.x += asteroid.velocity.x;
       asteroid.y += asteroid.velocity.y;
       asteroid.rotation += asteroid.rotationSpeed;
@@ -123,16 +134,18 @@ class Game {
   
   checkCollisions() {
     // Bullet-asteroid collisions
-    this.bullets.forEach((bullet, bulletIndex) => {
-      this.asteroids.forEach((asteroid, asteroidIndex) => {
+    for (let i = this.bullets.length - 1; i >= 0; i--) {
+      const bullet = this.bullets[i];
+      for (let j = this.asteroids.length - 1; j >= 0; j--) {
+        const asteroid = this.asteroids[j];
         const dx = bullet.x - asteroid.x;
         const dy = bullet.y - asteroid.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
         
         if (distance < asteroid.size) {
           // Remove bullet and asteroid
-          this.bullets.splice(bulletIndex, 1);
-          this.asteroids.splice(asteroidIndex, 1);
+          this.bullets.splice(i, 1);
+          this.asteroids.splice(j, 1);
           
           // Add score to player
           if (this.players[bullet.playerId]) {
@@ -140,28 +153,30 @@ class Game {
           }
           
           // Create new asteroids if needed
-          if (this.asteroids.length < 10) {
+          if (this.asteroids.length < 8) {
             this.createAsteroid();
           }
+          break;
         }
-      });
-    });
+      }
+    }
     
     // Player-asteroid collisions
     Object.keys(this.players).forEach(playerId => {
       const player = this.players[playerId];
-      this.asteroids.forEach((asteroid, index) => {
+      this.asteroids.forEach((asteroid) => {
         const dx = player.x - asteroid.x;
         const dy = player.y - asteroid.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
         
-        if (distance < asteroid.size + 10) {
-          player.health -= 10;
+        if (distance < asteroid.size + 15) {
+          player.health -= 5;
           if (player.health <= 0) {
             player.health = 100;
             player.x = Math.random() * this.gameWidth;
             player.y = Math.random() * this.gameHeight;
             player.velocity = { x: 0, y: 0 };
+            player.score = Math.max(0, player.score - 50);
           }
         }
       });
